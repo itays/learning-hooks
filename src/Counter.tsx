5caf150ae1002b22a32645535f58cbbd56903c20
Sidis, Itay (is874k) <is874k@intl.att.com>
2019-03-11 16:11:20 +0200

    ♻️ Share Logic Across Multiple React Components with Custom Hooks

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
 
+function useCounter(initialState: number = 0, step: number = 1) {
+  const [count, setCount] = useState(initialState);
+  const increment = () => setCount(count + step);
+  return {count, increment};
+}
+
 function Counter() {
-  const [count, setCount] = useState(0);
-  const increment = () => setCount(count + 1);
+  const {count, increment} = useCounter(0);
   return <button onClick={increment}>{count}</button>;
 }
 
