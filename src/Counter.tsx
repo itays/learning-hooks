1112e4f2dc991b4489a2c6e60004bf9fc8141c77
Sidis, Itay (is874k) <is874k@intl.att.com>
2019-03-11 16:52:48 +0200

    Store Values in localStorage with useEffect Hook

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-function useCounter(initialState: number = 0, step: number = 1) {
+function useCounter(initialState: any = 0, step: number = 1) {
   const [count, setCount] = useState(initialState);
   const increment = () => setCount(count + step);
   return {count, increment};
 }
 
 function Counter() {
-  const {count, increment} = useCounter(0);
+  const initialCount = () => Number(window.localStorage.getItem('count') || 0);
+  const {count, increment} = useCounter(initialCount);
+  useEffect(() => {
+    window.localStorage.setItem('count', count)
+  }, [count]);
   return <button onClick={increment}>{count}</button>;
 }
 
