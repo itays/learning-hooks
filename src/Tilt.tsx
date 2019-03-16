36714407a50db18bd8e6a599e7791ae7784e5157
Itay Sidis <itaysidis@gmail.com>
2019-03-16 21:44:29 +0200

    added useEffect hook to init vanillaTilt lib

--- src/Tilt.tsx
+++ src/Tilt.tsx
@@ -2,10 +2,11 @@ import React, { useRef, useEffect } from 'react';
 import VanillaTilt from 'vanilla-tilt';
 
 const Tilt: React.SFC<any> = (props: any) => {
-	const tiltRef = useRef<HTMLDivElement>(null);
+	const tiltRef = useRef<any>(null);
 	useEffect(() => {
-		VanillaTilt.init(tiltRef.current as HTMLElement, {});
-	});
+		VanillaTilt.init(tiltRef.current as HTMLElement);
+		return () => tiltRef.current.vanillaTilt.destroy();
+	}, []);
 	return (
 		<div ref={tiltRef} className="tilt-root">
 			<div className="tilt-child">{props.children}</div>
