5d34d6fb810a5074564f3fdda1ec0aed388529cd
Sidis, Itay (is874k) <is874k@intl.att.com>
2019-03-17 09:43:18 +0200

    Simplify a Component Reducer with the React setState Hook

--- src/Stopwatch.tsx
+++ src/Stopwatch.tsx
@@ -7,8 +7,8 @@ import React, {
 } from 'react';
 
 interface IStateType {
-	running: boolean;
-	lapse: number;
+	running?: boolean;
+	lapse?: number;
 }
 
 interface IActionType {
@@ -17,31 +17,32 @@ interface IActionType {
 	startTime?: number;
 }
 
-const reducer = (state: IStateType, action: IActionType): IStateType => {
-	switch (action.type) {
-		case 'LAPSE':
-			return {
-				...state,
-				lapse: action.now && action.startTime ? action.now - action.startTime : 0
-			};
-		case 'TOGGLE_RUNNING':
-			return {
-				...state,
-				running: !state.running
-			};
-		case 'CLEAR':
-			return {
-				...state,
-				running: false,
-				lapse: 0
-			};
-		default:
-			return state;
-	}
+const reducer = (currentState: IStateType, newState: IStateType): IStateType => {
+	return {...currentState, ...newState};
+	// switch (action.type) {
+	// 	case 'LAPSE':
+	// 		return {
+	// 			...state,
+	// 			lapse: action.now && action.startTime ? action.now - action.startTime : 0
+	// 		};
+	// 	case 'TOGGLE_RUNNING':
+	// 		return {
+	// 			...state,
+	// 			running: !state.running
+	// 		};
+	// 	case 'CLEAR':
+	// 		return {
+	// 			...state,
+	// 			running: false,
+	// 			lapse: 0
+	// 		};
+	// 	default:
+	// 		return state;
+	// }
 };
 
 const Stopwatch: FunctionComponent<any> = () => {
-	const [{ running, lapse }, dispatch] = useReducer(reducer, {
+	const [{ running, lapse }, setState] = useReducer(reducer, {
 		running: false,
 		lapse: 0
 	});
@@ -56,16 +57,16 @@ const Stopwatch: FunctionComponent<any> = () => {
 		if (running) {
 			clearStopwatch();
 		} else {
-			const startTime = Date.now() - lapse;
+			const startTime = Date.now() - (lapse || 0);
 			intervalRef.current = setInterval(() => {
-				dispatch({ type: 'LAPSE', now: Date.now(), startTime });
+				setState({ lapse: Date.now() - startTime});
 			}, 0);
 		}
-		dispatch({ type: 'TOGGLE_RUNNING' });
+		setState({ running: !running});
 	};
 	const onClear = () => {
 		clearStopwatch();
-		dispatch({ type: 'CLEAR' });
+		setState({ lapse: 0, running: false });
 	};
 	return (
 		<div style={{ textAlign: 'center' }}>
