3a2b9dcb8d077f3540f956062af91df6834d6d81
Itay Sidis <itaysidis@gmail.com>
2019-03-17 07:00:46 +0200

    Declaratively Interact with Complex Component State using the React useReducer Hook

--- src/Stopwatch.tsx
+++ src/Stopwatch.tsx
@@ -1,8 +1,50 @@
-import React, { useState, FunctionComponent, useRef, useEffect } from 'react';
+import React, {
+	useState,
+	FunctionComponent,
+	useRef,
+	useEffect,
+	useReducer
+} from 'react';
+
+interface IStateType {
+	running: boolean;
+	lapse: number;
+}
+
+interface IActionType {
+	type: string;
+	now?: number;
+	startTime?: number;
+}
+
+const reducer = (state: IStateType, action: IActionType): IStateType => {
+	switch (action.type) {
+		case 'LAPSE':
+			return {
+				...state,
+				lapse: action.now = action.startTime as number
+			};
+		case 'TOGGLE_RUNNING':
+			return {
+				...state,
+				running: !state.running
+			};
+		case 'CLEAR':
+			return {
+				...state,
+				running: false,
+				lapse: 0
+			};
+		default:
+			return state;
+	}
+};
 
 const Stopwatch: FunctionComponent<any> = () => {
-	const [lapse, setLapse] = useState(0);
-	const [running, setRunning] = useState(false);
+	const [{ running, lapse }, dispatch] = useReducer(reducer, {
+		running: false,
+		lapse: 0
+	} as never);
 	const intervalRef = useRef<NodeJS.Timeout | null>(null);
 	const clearStopwatch = () => {
 		clearInterval(intervalRef.current as NodeJS.Timeout);
@@ -16,15 +58,14 @@ const Stopwatch: FunctionComponent<any> = () => {
 		} else {
 			const startTime = Date.now() - lapse;
 			intervalRef.current = setInterval(() => {
-				setLapse(Date.now() - startTime);
+				dispatch({ type: 'LAPSE', now: Date.now(), startTime });
 			}, 0);
 		}
-		setRunning(!running);
+		dispatch({ type: 'TOGGLE_RUNNING' });
 	};
 	const onClear = () => {
 		clearStopwatch();
-		setLapse(0);
-		setRunning(false);
+		dispatch({ type: 'CLEAR' });
 	};
 	return (
 		<div style={{ textAlign: 'center' }}>
